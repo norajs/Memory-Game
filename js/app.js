d512ca5a7c9b61d6c533b4e9af31655594b31563
Nora Singer <norajsinger@gmail.com>
2018-10-21 15:09:33 -0400

    Flip card on click

--- js/app.js
+++ js/app.js
@@ -26,6 +26,7 @@ function shuffle(array) {
 }
 
 
+
 /*
  * set up the event listener for a card. If a card is clicked:
  *  - display the card's symbol (put this functionality in another function that you call from this one)
@@ -36,3 +37,16 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+
+// Select all cards and store in an array
+const cards = document.querySelectorAll('.card');
+// Watch for clicks on every card
+cards.forEach(function(element) {
+    element.addEventListener("click", function show() {
+    element.classList.add("open");
+    element.classList.add("show");
+    console.log(element.classList);
+    });
+});
+
+
